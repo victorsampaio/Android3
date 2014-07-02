779f70174ff037cb6c503ff1329c5acfb8ddf48b
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2014-07-02 01:14:28 -0300

    Android3
    Cont.2

---
+++ src/br/com/android3/c3/ExemploTres.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package br.com.android3.c3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * @author VictorSampaio
+ *
+ */
+public class ExemploTres extends Activity{
+	
+	@Override
+	public void onCreate(Bundle icicle){
+		super.onCreate(icicle);
+		
+		// Open screen 'layout_exemplo_tres'
+		setContentView(R.layout.layout_exemplo_tres);
+		
+		// Search the ImageView to id
+		ImageView img2 =(ImageView) findViewById(R.id.imagem2);
+		img2.setImageResource(R.drawable.smile2);
+		
+	}
+
+}
