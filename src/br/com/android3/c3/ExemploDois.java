3d2fc93d6571ed825ced2acaf96bdaf6af0eafb8
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2014-07-02 00:56:43 -0300

    Android3
    cont.1

--- src/br/com/android3/c3/ExemploDois.java
+++ src/br/com/android3/c3/ExemploDois.java
@@ -9,18 +9,20 @@ import android.widget.TextView;
 
 /**
  * @author VictorSampaio
- *
+ * 
  */
 public class ExemploDois extends Activity {
 	@Override
-	public void onCreate(Bundle icicle){
+	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.layout_exemplo_dois);
-		
-		// Create the 'TextView' to display on the screen 
+
+		// Create the 'TextView' to display on the screen
 		TextView view = new TextView(this);
-		view.setText("Exemplo de Texto no Android");
+		view.setText("Example using TextView on Android");
 		setContentView(view);
+
+		
 	}
 
 }
